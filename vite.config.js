01b6d13c033c6cb672b9cd758856760df4d4b60d
nostradamus01 <stepanyan0299@gmail.com>
2023-04-05 03:37:24 +0400

    some changes

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,34 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [
+    vue(),
+    ViteImageOptimizer({
+      test: /\.(jpe?g|png|webp)$/i,
+      includePublic: true,
+      logStats: true,
+      png: {
+        quality: 80,
+      },
+      jpeg: {
+        quality: 80,
+      },
+      jpg: {
+        quality: 80,
+      },
+      webp: {
+        lossless: true,
+      }
+    })
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    },
-  },
-  plugins: [vue()]
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
